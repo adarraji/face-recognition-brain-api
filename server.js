28aa1447d6ee2a8e44d5b44c7ff0065ae8e40f1c
adarraji <a.darraji@outlook.com>
2022-07-10 21:28:19 -0400

    added javascript object mock databse

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const express = require("express");
 const app = express();
 
 
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "john",
+            email: "john@gmail.com",
+            password: "cookies",
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: "124",
+            name: "Sally",
+            email: "sally@gmail.com",
+            password: "bananas",
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+
 app.listen("3000", () => {
     console.log("app is running on port 3000");
 })
