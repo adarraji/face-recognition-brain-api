1d259f707c342821ef1507238314d9f1187d4fb8
adarraji <a.darraji@outlook.com>
2022-07-14 17:40:28 -0400

    removed password from register return

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.post("/register", (req, res) => {
         id: "125",
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     });
