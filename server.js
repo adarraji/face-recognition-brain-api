4e6d23cc4d74eefd4100685bd8d792e7eb627f4d
adarraji <a.darraji@outlook.com>
2022-07-19 23:55:33 -0400

    verify id is number in /profile

--- server.js
+++ server.js
@@ -67,14 +67,18 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
     const { id } = req.params;
-    db.select("*").from("users").where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json("Error getting user")
-            }
-        })
+    if (!isNaN(id)) {
+        db.select("*").from("users").where({ id })
+            .then(user => {
+                if (user.length) {
+                    res.json(user[0]);
+                } else {
+                    res.status(400).json("Error getting user");
+                }
+            })
+    } else {
+        res.status(400).json("Not a valid id");
+    }
 });
 
 app.put("/image", (req, res) => {
