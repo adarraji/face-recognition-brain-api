28fe22ae8958e91ec10b12d34ff5dba1f4317b42
adarraji <a.darraji@outlook.com>
2022-07-20 00:01:49 -0400

    updated /profile

--- server.js
+++ server.js
@@ -67,17 +67,13 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
     const { id } = req.params;
-    if (!isNaN(id)) {
-        db.select("*").from("users").where({ id }).then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json("Error getting user");
-            }
-        })
-    } else {
-        res.status(400).json("Not a valid id");
-    }
+    db.select("*").from("users").where({ id }).then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json("Not found");
+        }
+    }).catch(err => res.status(400).json("Error getting user"));
 });
 
 app.put("/image", (req, res) => {
