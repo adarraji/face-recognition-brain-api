567a58201b9ee26279b98c1bb3de47a4f33b9f8f
adarraji <a.darraji@outlook.com>
2022-07-20 11:57:21 -0400

    Added knex query to /image

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config();
-const knex = require('knex')
+const knex = require('knex');
+const { response } = require("express");
 
 const db = knex({
     client: 'pg',
@@ -78,17 +79,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(400).json("not found");
-    }
+    db("users")
+        .where("id", '=', id)
+        .increment("entries", 1)
+        .returning("entries")
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json("Error getting entries"));;
 });
 
 
