ce866e2d45983ab88c631c2ef081e327e214c8bf
adarraji <a.darraji@outlook.com>
2022-07-20 13:06:03 -0400

    add trx to /resgiter to insert into users & login

--- server.js
+++ server.js
@@ -58,15 +58,25 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
     const { name, email, password } = req.body;
-    db("users").returning("*").insert({
-        name: name,
-        email: email,
-        joined: new Date()
-    })
-        .then(user => {
-            res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .catch(err => res.status(400).json("unable to register"));
+            .into("login")
+            .returning("email")
+            .then(loginEmail => {
+                trx("users").returning("*").insert({
+                    name: name,
+                    email: loginEmail[0].email,
+                    joined: new Date()
+                })
+                    .then(user => res.json(user[0]))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    }).catch(err => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
