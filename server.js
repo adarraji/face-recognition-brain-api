d44f7684a06540e962453bb54dc0230358b0984d
adarraji <a.darraji@outlook.com>
2022-07-19 23:50:15 -0400

    updated /profile to use DB

--- server.js
+++ server.js
@@ -67,16 +67,14 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    });
-    if (!found) {
-        res.status(400).json("not found");
-    }
+    db.select("*").from("users").where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json("Error getting user")
+            }
+        })
 });
 
 app.put("/image", (req, res) => {
