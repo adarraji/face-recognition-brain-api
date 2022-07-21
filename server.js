0c098ddb51e80af0118e251dc0a2bcaea9c14f1a
adarraji <a.darraji@outlook.com>
2022-07-20 20:52:25 -0400

    Added knex query to /signin

--- server.js
+++ server.js
@@ -23,37 +23,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: "123",
-            name: "john",
-            email: "john@gmail.com",
-            password: "cookies",
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: "124",
-            name: "Sally",
-            email: "sally@gmail.com",
-            password: "bananas",
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get("/", (req, res) => {
     res.json(database.users);
 });
 
 app.post("/signin", (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json("error logging in");
-    }
+    db.select("email", "hash").from("login")
+        .where("email", "=", req.body.email)
+        .then(data => {
+            const isvalid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isvalid) {
+                return db.select("*").from("users").where("email", "=", req.body.email)
+                    .then(user => res.json(user[0]))
+                    .catch(err => res.status(400).json("unable to get user"));
+            } else {
+                res.status(400).json("wrong credentials");
+            }
+
+        })
+        .catch(err => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
