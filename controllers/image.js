7bd414ac7e104916beae6e6af1d76ff8a6444901
adarraji <a.darraji@outlook.com>
2022-07-22 17:17:43 -0400

    added clarifai code and new route

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require("clarifai");
+require('dotenv').config();
+
+// Add your own API key here from Clarifai
+const app = new Clarifai.App({
+    apiKey: `${process.env.REACT_APP_API_KEY}`
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json("unable to work with API"));
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db("users")
@@ -11,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
-const Clarifai = require("clarifai");
 
 require('dotenv').config();
 
@@ -35,6 +34,7 @@ app.post("/signin", (req, res) => signin.handleSignIn(req, res, db, bcrypt));
 app.post("/register", (req, res) => register.handleRegister(req, res, db, bcrypt));
 app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
 app.put("/image", (req, res) => image.handleImage(req, res, db));
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
 
 app.listen("3000", () => {
