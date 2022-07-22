c9c34550605d9cf11c76b05a09745b0f92e0656f
adarraji <a.darraji@outlook.com>
2022-07-21 21:28:46 -0400

    added form validation in signin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,10 +1,14 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json("invalid form submission");
+    }
     db.select("email", "hash").from("login")
-        .where("email", "=", req.body.email)
+        .where("email", "=", email)
         .then(data => {
-            const isvalid = bcrypt.compareSync(req.body.password, data[0].hash);
+            const isvalid = bcrypt.compareSync(password, data[0].hash);
             if (isvalid) {
-                return db.select("*").from("users").where("email", "=", req.body.email)
+                return db.select("*").from("users").where("email", "=", email)
                     .then(user => res.json(user[0]))
                     .catch(err => res.status(400).json("unable to get user"));
             } else {
