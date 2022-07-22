1ee1b1cc226600901ed7c2cf4cfe0d1afe92d0f8
adarraji <a.darraji@outlook.com>
2022-07-21 21:13:29 -0400

    added form validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {
     const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json("invalid form submission");
+    }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
