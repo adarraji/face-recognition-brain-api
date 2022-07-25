34595ce15b7facea223f7d56cff188843551b973
adarraji <a.darraji@outlook.com>
2022-07-24 21:38:16 -0400

    updated app.listen message

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
 
 app.listen(process.env.PORT || "3000", () => {
-    console.log("app is running on port 3000");
+    console.log(`app is running on port ${process.env.PORT}`);
 });
